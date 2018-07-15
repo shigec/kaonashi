fa5378b947bd658b3f8c0ba00fb214b9beb943af
Y-Suzaki <geranium04.24.1981@gmail.com>
2018-07-15 19:36:48 +0900

    IAM Credentialを使うため、ローカルビルド時のデプロイスクリプトを分離

--- deploy-local.sh
+++ deploy-local.sh
@@ -12,17 +12,20 @@ cd ..
 
 echo "Upload applicaiton zip and swagger spec to s3 bucket..."
 aws s3 cp kaonashi-function.zip s3://${s3_bucket}/ \
-  --region us-west-2
+  --region us-west-2 \
+  --profile default 
 
 aws s3 cp api/swagger.yaml s3://${s3_bucket}/ \
-  --region us-west-2
+  --region us-west-2 \
+  --profile default 
 
 echo "Build kaonashi api..."
 aws cloudformation package \
   --template-file aws-sam.yaml \
   --output-template-file aws-sam-deploy.yaml \
   --s3-bucket ${s3_bucket} \
-  --region us-west-2 
+  --region us-west-2 \
+  --profile default 
 
 echo "Deploy kaonashi api..."
 aws cloudformation deploy \
@@ -30,7 +33,8 @@ aws cloudformation deploy \
   --stack-name kaonash-aws-sam-deploy-dev \
   --capabilities CAPABILITY_IAM \
   --parameter-overrides ArtifactBucket=${s3_bucket} \
-  --region us-west-2 
+  --region us-west-2 \
+  --profile default 
 
 echo "** All complete! **"
 rm -rf deploy
